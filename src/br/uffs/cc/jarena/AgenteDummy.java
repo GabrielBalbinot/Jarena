1712d66ba0cd5ddaf6e1e8640bb97fec6cc6e03b
Gabriel Balbinot <gabrielfdr.balbinot@gmail.com>
2025-10-11 18:20:27 -0300

    Criado atributos e métodos para definir o quadrante em que o agente irá se movimentar, juntamente com atributos para definir os limites destes quadrantes. Criado um protótipo de movimentação e implementado no método pensa(), onde o agente irá se movimentar de maneira diagonal até bater em algum dos limites, mudando assim a direção que está se locomovendo (descendo/subindo, indo para direita/esquerda)

--- src/br/uffs/cc/jarena/AgenteDummy.java
+++ src/br/uffs/cc/jarena/AgenteDummy.java
@@ -3,38 +3,47 @@
  * para a criação de um agente mais esperto. Para mais informações sobre métodos que podem
  * ser utilizados, veja a classe Agente.java.
  * 
- * Fernando Bevilacqua <fernando.bevilacqua@uffs.edu.br>
+ * Gabriel Francisco Dall Rosa Balbinot <gabriel.balbinot@estudante.uffs.edu.br>
  */
 
 package br.uffs.cc.jarena;
 
 public class AgenteDummy extends Agente
 {
+	private static int qtdAgentes = 0;
+
+	private int quadrante;
+	private int[] limitesHorizontas = new int[2];
+	private int[] limitesVerticais = new int[2];
+
+	private boolean descendo;
+	private boolean indoParaDireita;
+
 	public AgenteDummy(Integer x, Integer y, Integer energia) {
 		super(x, y, energia);
-		setDirecao(geraDirecaoAleatoria());
+		qtdAgentes++;
+
+		this.descendo = setDescida();
+		this.indoParaDireita = setDireita();
+
+		this.quadrante = setQuadrante(x, y);
+		setLimitesDoQuadrante(quadrante);
+
+		//setDirecao(geraDirecaoAleatoria());
 	}
 	
 	public void pensa() {
-		// Se não conseguimos nos mover para a direção atual, quer dizer
-		// que chegamos no final do mapa ou existe algo bloqueando nosso
-		// caminho.
-		if(!podeMoverPara(getDirecao())) {
-			// Como não conseguimos nos mover, vamos escolher uma direção
-			// nova.
-			setDirecao(geraDirecaoAleatoria());
-		}
-		
-		// Se o agente conseguie se dividir (tem energia) e se o total de energia
-		// do agente é maior que 400, nos dividimos. O agente filho terá a metade
-		// da nossa energia atual.
-		if(podeDividir() && getEnergia() >= 800) {
+
+		mudarMovimentacao();
+			
+		if(podeDividir() && getEnergia() >= 2000) {
 			divide();
 		}
 	}
 	
 	public void recebeuEnergia() {
 		// Invocado sempre que o agente recebe energia.
+		super.para();
 	}
 	
 	public void tomouDano(int energiaRestanteInimigo) {
@@ -52,6 +61,132 @@ public class AgenteDummy extends Agente
 	
 	public String getEquipe() {
 		// Definimos que o nome da equipe do agente é "Fernando".
-		return "Fernando";
+		return "Gabriel Balbinot";
 	}
+
+	private int setQuadrante(int x, int y) {
+
+		final int centroX = Constants.LARGURA_MAPA/2;
+		final int centroY = Constants.ALTURA_MAPA/2;
+		
+		if (x >= centroX) {
+
+			if (y <= centroY) {
+				return 1;
+			} else {
+				return 4;
+			}
+
+		} else {
+			if (y <= centroY) {
+				return 2;
+			} else {
+				return 3;
+			}
+		}
+
+	}
+
+	private void setLimitesDoQuadrante(int quadrante) {
+
+		if (quadrante == 1) {
+			limitesHorizontas[0] = Constants.LARGURA_MAPA/2;
+			limitesHorizontas[1] = Constants.LARGURA_MAPA;
+			limitesVerticais[0] = 0;
+			limitesVerticais[1] = Constants.ALTURA_MAPA/2; 
+		} else if (quadrante == 2) {
+			limitesHorizontas[0] = 0;
+			limitesHorizontas[1] = Constants.LARGURA_MAPA/2;
+			limitesVerticais[0] = 0;
+			limitesVerticais[1] = Constants.ALTURA_MAPA/2; 
+		} else if (quadrante == 3) {
+			limitesHorizontas[0] = 0;
+			limitesHorizontas[1] = Constants.LARGURA_MAPA/2;
+			limitesVerticais[0] = Constants.ALTURA_MAPA/2;
+			limitesVerticais[1] = Constants.ALTURA_MAPA; 
+		} else {
+			limitesHorizontas[0] = Constants.LARGURA_MAPA/2;
+			limitesHorizontas[1] = Constants.LARGURA_MAPA;
+			limitesVerticais[0] = Constants.ALTURA_MAPA/2;
+			limitesVerticais[1] = Constants.ALTURA_MAPA; 
+		}
+
+
+	}
+
+	private void bateuNosLimites(int x, int y) {
+		if (x >= limitesHorizontas[1]) {			
+			this.indoParaDireita = false;
+
+		} else if (x <= limitesHorizontas[0]) {
+			
+			this.indoParaDireita = true;
+		}
+
+		if (y >= limitesVerticais[1]) {
+			
+			this.descendo = false;
+		} else if (y <= limitesVerticais[0]) {
+			
+			this.descendo = true;
+		}
+	}
+
+	private void mudarMovimentacao() {
+		bateuNosLimites(getX(), getY());
+
+		if (this.descendo && this.indoParaDireita) {
+
+			if (getDirecao() == DIREITA) {
+				setDirecao(BAIXO);
+			} else {
+				setDirecao(DIREITA);
+			}
+
+		} else if (!this.descendo && this.indoParaDireita) {
+
+			if (getDirecao() == DIREITA) {
+				setDirecao(CIMA);
+			} else {
+				setDirecao(DIREITA);
+			}
+
+		} else if (this.descendo && !this.indoParaDireita) {
+
+			if (getDirecao() == ESQUERDA) {
+				setDirecao(BAIXO);
+			} else {
+				setDirecao(ESQUERDA);
+			}
+
+		} else if (!this.descendo && !this.indoParaDireita) {
+
+			if (getDirecao() == ESQUERDA) {
+				setDirecao(CIMA);
+			} else {
+				setDirecao(ESQUERDA);
+			}
+
+		}
+		
+	}
+
+	private boolean setDescida() {
+		if (qtdAgentes % 2 == 0) {
+			return true;
+		}
+
+		return false;
+	}
+
+	private boolean setDireita() {
+		if (qtdAgentes % 2 == 0) {
+			return false;
+		}
+
+		return true;
+	}
+
 }
+
+
